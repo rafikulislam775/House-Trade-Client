91690bea69554d441ab37bdbf605f1a505b0ff4f
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-12-02 17:48:53 +0900

    added delte function on users information

--- src/AdminDashboard/AllUsers.jsx
+++ src/AdminDashboard/AllUsers.jsx
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosPublic from "../api/axiosInstance";
 import { RiDeleteBin2Fill } from "react-icons/ri";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axios = axiosPublic;
-  const { data: users = [] } = useQuery({
+  const { data: users = [] , refetch,isLoading} = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axios.get("/users");
@@ -12,8 +13,8 @@ const AllUsers = () => {
     },
   });
 
-  const handleDelate = (id) => {
-    console.log(id);
+  const handleDelate = (user) => {
+
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -24,7 +25,7 @@ const AllUsers = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        axios.delete(`/wishlist/${id}`).then((res) => {
+        axios.delete(`/users/${user.id}`).then((res) => {
           if (res.data.deletedCount > 0) {
             refetch();
             Swal.fire({
@@ -61,7 +62,7 @@ const AllUsers = () => {
                 <td>Blue</td>
                 <td>
                   <button
-                    onClick={() => handleDelate(item._id)}
+                    onClick={() => handleDelate(user)}
                     className="btn btn-sm text-white bg-red-600"
                   >
                     <RiDeleteBin2Fill></RiDeleteBin2Fill>
