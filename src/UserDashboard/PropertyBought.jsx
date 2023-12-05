40e88e18f5610b5ebcd5b9cc3978265911559dc1
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-12-06 07:14:41 +0900

    added property bought pages

--- src/UserDashboard/PropertyBought.jsx
+++ src/UserDashboard/PropertyBought.jsx
@@ -1,9 +1,22 @@
-
-
+import { Link } from "react-router-dom";
+import { TbPaperBagOff } from "react-icons/tb";
 const PropertyBought = () => {
   return (
     <div>
-      <h2>prsdifsdkgvh</h2>
+      <section className="flex items-center h-full sm:p-16  text-black">
+        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
+          <TbPaperBagOff className="text-8xl"></TbPaperBagOff>
+          <p className="text-3xl">
+            Looks like you have not bought any property yet!
+          </p>
+          <Link
+            to="/"
+            className="px-8 py-3 font-semibold rounded dark:bg-orange-500 text-white hover:animate-pulse"
+          >
+            Back to homepage
+          </Link>
+        </div>
+      </section>
     </div>
   );
 };
